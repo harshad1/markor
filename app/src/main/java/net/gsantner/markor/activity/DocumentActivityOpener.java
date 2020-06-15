8023a3cc492bc0d71a9d087214c7e64254083897
Harshad Srinivasan <harshad1@zoho.com>
2020-06-14 23:02:38 -0700

    Reverted some changes to master

---
+++ app/src/main/java/net/gsantner/markor/activity/DocumentActivityOpener.java
@@ -0,0 +1,12 @@
+package net.gsantner.markor.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DocumentActivityOpener extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
