445cd30482bd51f268583b1fe479ff0799151287
Li Guanglin <2311987902@qq.com>
2024-05-24 08:09:36 +0800

    Make headline dialog wider in portrait mode

--- app/src/main/java/net/gsantner/markor/frontend/MarkorDialogFactory.java
+++ app/src/main/java/net/gsantner/markor/frontend/MarkorDialogFactory.java
@@ -874,7 +874,7 @@ public class MarkorDialogFactory {
             GsSearchOrCustomTextDialog.showMultiChoiceDialogWithSearchFilterUI(activity, dopt2);
         };
 
-        dopt.portraitAspectRatio = new float[]{0.95f, 0.8f};
+        dopt.portraitAspectRatio = new float[]{1.0f, 0.8f};
         dopt.landscapeAspectRatio = new float[]{0.7f, 0.95f};
         dopt.gravity = Gravity.CENTER;
         dopt.dismissCallback = (d) -> {
